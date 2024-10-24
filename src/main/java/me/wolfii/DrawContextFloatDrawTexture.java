3a9903ab28c6458b012199eb796faaf6de51a598
haykam821 <24855774+haykam821@users.noreply.github.com>
2024-10-24 16:30:22 -0400

    Update mod to Minecraft 1.21.3

--- src/main/java/me/wolfii/DrawContextFloatDrawTexture.java
+++ src/main/java/me/wolfii/DrawContextFloatDrawTexture.java
@@ -1,7 +1,10 @@
 package me.wolfii;
 
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.util.Identifier;
 
+import java.util.function.Function;
+
 public interface DrawContextFloatDrawTexture {
-    default void centered_crosshair$drawTexture(Identifier texture, float x, float y, int width, int height) {}
+    default void centered_crosshair$drawTexture(Function<Identifier, RenderLayer> renderLayers, Identifier texture, float x, float y, int width, int height) {}
 }
